e8ce0e8cbf0dd5ea834a5b1e62bd290579842d03
MF-Jiang <shujun2002@outlook.com>
2023-09-03 14:27:35 +0800

    第14题

---
+++ 643子数组最大平均数I/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int sum = 0;
+        int n = nums.size();
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < n; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = max(maxSum, sum);
+        }
+        return static_cast<double>(maxSum) / k;
+    }
+};

917edfa0e79c846850cd84eafc82bf611ea0bbc1
MF-Jiang <shujun2002@outlook.com>
2023-09-11 21:30:42 +0800

    第41、42题

---
+++ 450删除二叉搜索树中的节点/Solution.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        TreeNode *cur = root, *curParent = nullptr;
+        while (cur && cur->val != key) {
+            curParent = cur;
+            if (cur->val > key) {
+                cur = cur->left;
+            } else {
+                cur = cur->right;
+            }
+        }
+        if (!cur) {
+            return root;
+        }
+        if (!cur->left && !cur->right) {
+            cur = nullptr;
+        } else if (!cur->right) {
+            cur = cur->left;
+        } else if (!cur->left) {
+            cur = cur->right;
+        } else {
+            TreeNode *successor = cur->right, *successorParent = cur;
+            while (successor->left) {
+                successorParent = successor;
+                successor = successor->left;
+            }
+            if (successorParent->val == cur->val) {
+                successorParent->right = successor->right;
+            } else {
+                successorParent->left = successor->right;
+            }
+            successor->right = cur->right;
+            successor->left = cur->left;
+            cur = successor;
+        }
+        if (!curParent) {
+            return cur;
+        } else {
+            if (curParent->left && curParent->left->val == key) {
+                curParent->left = cur;
+            } else {
+                curParent->right = cur;
+            }
+            return root;
+        }
+    }
+};
+
---
+++ 700二叉搜索树中的搜索/Solution.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *searchBST(TreeNode *root, int val) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        if (val == root->val) {
+            return root;
+        }
+        return searchBST(val < root->val ? root->left : root->right, val);
+    }
+};

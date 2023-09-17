42ac4298a2f0832eb70df46de19a16cf31646377
MF-Jiang <shujun2002@outlook.com>
2023-09-17 11:32:42 +0800

    第59、60、61、62题

---
+++ 198打家劫舍/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        int size = nums.size();
+        if (size == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = max(nums[0], nums[1]);
+        for (int i = 2; i < size; i++) {
+            int temp = second;
+            second = max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+};
